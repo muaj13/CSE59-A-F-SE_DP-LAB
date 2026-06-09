312b73d088600a204aa420a0da2c28322529d58d
muaj13 <156597416+muaj13@users.noreply.github.com>
2026-06-09 11:18:57 +0600

    Add ContestState class to manage scores

--- Task 2/YouDo1.java
+++ Task 2/YouDo1.java
@@ -1,1 +1,15 @@
+class ContestState {
+    int[] scores = {85,90,95};
 
+
+void PrintFirstScore() {
+    System.out.println(scores[0]);
+   }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ContestState contest = new ContestState();
+        contest.PrintFirstScore();
+    }
+}
