8758b6d84b2a7bedb9a55632deb86be5f5e1d82f
muaj13 <156597416+muaj13@users.noreply.github.com>
2026-06-30 11:40:10 +0600

    Add quantization models and interface youdo4
    Implemented quantization models for ResNet18 and MobileNet.

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,34 @@
+interface QuantizationModel {
+    void quantizeModel();
+}
+class ResNet18 implements QuantizationModel {
+    @Override
+    public void quantizeModel() {
+        System.out.println("applying int18");
+    }
+}
 
+class MobileNet implements QuantizationModel {
+    @Override
+    public void quantizeModel() {
+        System.out.println("applying int8");
+    }
+
+}
+
+class InterfaceEngine {
+    public void applyQuantization(QuantizationModel model) {
+        model.quantizeModel();
+    }
+}
+
+public class Main4 {
+    public static void main(String[] args) {
+        InterfaceEngine engine = new InterfaceEngine();
+        QuantizationModel rsNet = new ResNet18();
+        QuantizationModel mobileNet = new MobileNet();
+
+        engine.applyQuantization(rsNet);
+        engine.applyQuantization(mobileNet);
+    }
+} 
